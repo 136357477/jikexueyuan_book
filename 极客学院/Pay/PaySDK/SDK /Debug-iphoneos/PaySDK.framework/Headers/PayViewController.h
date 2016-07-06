ebf3045a1606612a876826d24845ac52c04e9e30
guohui <guohui136357477@126.com>
2016-07-06 19:50:25 +0800

    之前做的极客学院项目整理

---
+++ 极客学院/Pay/PaySDK/SDK /Debug-iphoneos/PaySDK.framework/Headers/PayViewController.h
@@ -0,0 +1,17 @@
+//
+//  PayViewController.h
+//  PaySDK
+//
+//  Created by guohui on 16/5/4.
+//  Copyright © 2016年 guohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PayViewController : UIViewController
+
++ (void)startPay ;
+
+
+
+@end
---
+++ 极客学院/Pay/PaySDK/SDK /Release-iphonesimulator/include/PaySdk/PaySdk.h
@@ -0,0 +1,13 @@
+//
+//  PaySdk.h
+//  PaySdk
+//
+//  Created by guohui on 16/5/3.
+//  Copyright © 2016年 guohui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PaySdk : NSObject
++ (void)startPay;
+@end
